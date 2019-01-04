c22d74d83280829baa80a83302d77dafc17c034a
Destrella3 <destrella385@gmail.com>
2019-01-03 20:58:10 -0500

    made some asthetic changes

--- Pursuit-Core-iOS-Unit3-Holiday-Assignment/Contollers/MonsterHunterWeaponController.swift
+++ Pursuit-Core-iOS-Unit3-Holiday-Assignment/Contollers/MonsterHunterWeaponController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class MonsterHunterWeaponController: UIViewController {
-
+    
     private var monsterHunterWeapons = [MonsterHunterWeapon]() {
         didSet {
             DispatchQueue.main.async {
@@ -21,23 +21,23 @@ class MonsterHunterWeaponController: UIViewController {
         loadData()
     }
     func loadData() {
+        mhWeaponTableView.layer.borderWidth = 2.0
+        title = "Monster Hunter World Weapon List"
         MonsterHunterWeaponsAPIClient.getAllWeapons { (weapon, error) in
-    if let weapon = weapon {
-    self.monsterHunterWeapons = weapon
+            if let weapon = weapon {
+                self.monsterHunterWeapons = weapon
+            }
+            if let error = error {
+                print(error)
+            }
+        }
     }
-    if let error = error {
-    print(error)
-    }
-}
-    mhWeaponTableView.layer.borderWidth = 2.0
-    title = "Monster Hunter Weapon List"
-}
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destination = segue.destination as? MonsterHunterWeaponsDetailViewController,
             let cellSelected = mhWeaponTableView.indexPathForSelectedRow else { return }
-            let userSelected = monsterHunterWeapons[cellSelected.row]
-            destination.mhWeapon = userSelected
+        let userSelected = monsterHunterWeapons[cellSelected.row]
+        destination.mhWeapon = userSelected
     }
 }
 
@@ -50,12 +50,12 @@ extension MonsterHunterWeaponController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = mhWeaponTableView.dequeueReusableCell(withIdentifier: "mhWeaponCell", for: indexPath) as? MonsterHunterWeaponCell else { return UITableViewCell() }
         let weaponToSet = monsterHunterWeapons[indexPath.row]
-                do {
-                    let imageData = try Data(contentsOf: (weaponToSet.assets?.icon)!)
-                    cell.mhWeaponImage.image = UIImage.init(data: imageData)
-                } catch {
-                    print("Error: \(error)")
-                }
+        do {
+            let imageData = try Data(contentsOf: (weaponToSet.assets?.icon)!)
+            cell.mhWeaponImage.image = UIImage.init(data: imageData)
+        } catch {
+            print("Error: \(error)")
+        }
         cell.mhWeaponName.text = weaponToSet.name
         cell.layer.borderWidth = 2
         return cell
@@ -66,8 +66,8 @@ extension MonsterHunterWeaponController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         guard let searchText = searchBar.text,
-        !searchText.isEmpty,
+            !searchText.isEmpty,
             let searchTextEncoded = searchText.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return loadData()}
-    monsterHunterWeapons = monsterHunterWeapons.filter{$0.name.contains(searchTextEncoded.capitalized)}
+        monsterHunterWeapons = monsterHunterWeapons.filter{$0.name.contains(searchTextEncoded.capitalized)}
     }
 }
--- Pursuit-Core-iOS-Unit3-Holiday-Assignment/Contollers/MonsterHunterWeaponsDetailViewController.swift
+++ Pursuit-Core-iOS-Unit3-Holiday-Assignment/Contollers/MonsterHunterWeaponsDetailViewController.swift
@@ -8,6 +8,7 @@ class MonsterHunterWeaponsDetailViewController: UIViewController {
     @IBOutlet weak var weaponName: UILabel!
     @IBOutlet weak var weaponType: UILabel!
     @IBOutlet weak var weaponDamage: UILabel!
+    @IBOutlet weak var damageType: UILabel!
     
     
     override func viewDidLoad() {
@@ -18,6 +19,7 @@ class MonsterHunterWeaponsDetailViewController: UIViewController {
     func updateInfo() {
         weaponName.text = mhWeapon.name
         weaponType.text = mhWeapon.type.capitalized
+        damageType.text = "Damage Type: \(mhWeapon.attributes.damageType?.capitalized ?? "")"
         weaponDamage.text = "Weapon Damage: \(mhWeapon.attack.raw)"
         do {
             let imageData = try Data(contentsOf: (mhWeapon.assets?.image)!)
@@ -25,5 +27,7 @@ class MonsterHunterWeaponsDetailViewController: UIViewController {
         } catch {
             print("Error: \(error)")
         }
+        self.weaponImage.layer.cornerRadius = self.weaponImage.frame.size.width / 2
+        self.weaponImage.clipsToBounds = true
     }
 }
